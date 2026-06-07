2cf2de34c7a9e190af674c15a98056ec4c1298c6
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-06-07 20:17:03 +0530

    Updated Files

--- spiralMatrix.cpp
+++ spiralMatrix.cpp
@@ -39,9 +39,9 @@ void spiralMatrix(int matrix[][4],int n, int m){
 
 int main(){
     int matrix[4][4]= { {1,2,3,4},
-      {5,6,7,8},
-      {9,10,11,12},
-      {13,14,15,16} };
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16} };
       
       spiralMatrix(matrix,4,4);
       return 0;
