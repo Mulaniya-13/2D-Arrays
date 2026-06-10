6c2a84e9125308c8a1bdaaba61a070432c297712
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-06-10 13:59:48 +0530

    Adding new Files

--- matrixPointers.cpp
+++ matrixPointers.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
 
+void func(int mat[][4],int n,int m){
+    // cout<<"0th row ptr = "<<mat<<endl;
+    // cout<<"1st row ptr = "<<mat+1<<endl;
+    // cout<<"2nd row ptr = "<<mat+2<<endl;
+
+    // cout<<"0th row value = "<<*mat<<endl;
+    // cout<<"1st row value = "<<*(mat+1)<<endl;
+    // cout<<"2nd row value = "<<*(mat+2)<<endl;
+
+    cout<<*(*(mat+2)+2)<<endl;
+}
+
+void func2(int (*mat)[4],int n,int m){
+    
+}
+
 int main(){
     int matrix[4][4]={ {1,2,3,4},
                        {5,6,7,8},
@@ -8,8 +24,10 @@ int main(){
                        {13,14,15,16}
                      };
     
-    cout<<matrix<<" = "<<&matrix[0][0]<<endl;
-    cout<<matrix+1<<" != "<<&matrix[0][1]<<endl;
-    cout<<matrix+1<<" = "<<&matrix[1][0]<<endl;
+    // cout<<matrix<<" = "<<&matrix[0][0]<<endl;
+    // cout<<matrix+1<<" != "<<&matrix[0][1]<<endl;
+    // cout<<matrix+1<<" = "<<&matrix[1][0]<<endl;
+
+    func(matrix,4,4);
     return 0;             
 }
